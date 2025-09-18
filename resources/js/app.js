88734c181f5ac0aae634214f80040d5150a9be50
Issufi Badji <45535344+issufibadji@users.noreply.github.com>
2025-09-18 18:41:05 -0300

    Add Inertia Vue setup and root view

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,16 @@
 import './bootstrap';
+import { createApp, h } from 'vue';
+import { createInertiaApp } from '@inertiajs/vue3';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 
-import Alpine from 'alpinejs';
+const appName = window.document.getElementsByTagName('title')[0]?.innerText ?? 'Laravel';
 
-window.Alpine = Alpine;
-
-Alpine.start();
+createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) => resolvePageComponent(`./pages/${name}.vue`, import.meta.glob('./pages/**/*.vue')),
+    setup({ el, App, props, plugin }) {
+        createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .mount(el);
+    },
+});
